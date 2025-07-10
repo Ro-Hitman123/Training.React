58218f1e75bb24c25c1cccbc8bb3c42d377a820d
Ro-Hitman123 <khanalkritagya29@gmail.com>
2025-07-10 11:00:08 +0545

    initial commit

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -4,7 +4,7 @@ import Banner from '../components/UI/Banner'
 function  About() {
   return (
  <div>
-    <Banner title=" my page" subtitle="It's me Kritagya Khanal"
+    <Banner title=" my page" subtitle="BSc.CSIT Student"
         description="it's a description"/>
  </div>
   )
